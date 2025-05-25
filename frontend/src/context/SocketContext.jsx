dd5af281645b51aa9446e7c35e153c48b4223df7
Nishant <nishant.dwivedi237@gmail.com>
2025-05-26 02:01:14 +0530

    Deployment completed!

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authuser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://wechat-8o7y.onrender.com", {
 				query: {
 					userId: authuser._id,
 				},
